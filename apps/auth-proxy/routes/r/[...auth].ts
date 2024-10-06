bcd35798f8d82102809181f18e2432a2171ff06d
James Devereux <devereuxjj@gmail.com>
2024-10-06 20:52:44 +0100

    add log statements in auth

--- apps/auth-proxy/routes/r/[...auth].ts
+++ apps/auth-proxy/routes/r/[...auth].ts
@@ -2,17 +2,22 @@ import { Auth } from "@auth/core";
 import Discord from "@auth/core/providers/discord";
 import { eventHandler, toWebRequest } from "h3";
 
-export default eventHandler(async (event) =>
-  Auth(toWebRequest(event), {
-    basePath: "/r",
-    secret: process.env.AUTH_SECRET,
-    trustHost: !!process.env.VERCEL,
-    redirectProxyUrl: process.env.AUTH_REDIRECT_PROXY_URL,
-    providers: [
-      Discord({
-        clientId: process.env.AUTH_DISCORD_ID,
-        clientSecret: process.env.AUTH_DISCORD_SECRET,
-      }),
-    ],
-  }),
-);
+export default eventHandler(async (event) => {
+
+  console.log("Incoming request headers:", event.headers);
+  console.log("Incoming request cookies:", event.headers.get('cookie'));
+
+    Auth(toWebRequest(event), {
+      basePath: "/r",
+      secret: process.env.AUTH_SECRET,
+      trustHost: !!process.env.VERCEL,
+      redirectProxyUrl: process.env.AUTH_REDIRECT_PROXY_URL,
+      providers: [
+        Discord({
+          clientId: process.env.AUTH_DISCORD_ID,
+          clientSecret: process.env.AUTH_DISCORD_SECRET,
+        }),  
+      ],  
+    })  
+})    
+
